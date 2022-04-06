f9294449014fab57a1572b01c4c48daed46bc4ea
JeykerSalinas <66428501+JeykerSalinas@users.noreply.github.com>
2022-04-06 13:49:59 -0400

    Filter by categories and by name

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
+const { Console } = require("console");
 const app = express();
 const PORT = 3600;
 app.use(bodyParser.json());
@@ -41,6 +42,33 @@ app.get("/bsale/products", (req, res) => {
 //Getting categories
 app.get("/bsale/category/", (req, res) => {
   const sql = `SELECT * FROM category`;
+  conection.query(sql, (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      res.send("No se encontraron resultados");
+    }
+  });
+});
+//Filter by categories
+app.get("/bsale/products/category/:id", (req, res) => {
+  console.log("req: ", req.params);
+  const sql = `SELECT * FROM product WHERE category = ${req.params.id}`;
+  conection.query(sql, (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      res.json(results);
+    } else {
+      res.send("No se encontraron resultados");
+    }
+  });
+});
+
+//Filter by name
+app.get("/bsale/products/name/:name", (req, res) => {
+  console.log("req: ", req.params);
+  const sql = `SELECT * FROM product WHERE name LIKE '%${req.params.name}%'`;
   conection.query(sql, (err, results) => {
     if (err) throw err;
     if (results.length > 0) {
